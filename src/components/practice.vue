6a2d10e1b3590fe9ded356d6c1a955447f72deb8
Steven Berk <steven.e.berk@gmail.com>
2018-09-03 20:57:00 -0400

    first commit of VueJS practice

---
+++ src/components/practice.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+  <div class="holder">
+    <ul>
+      <li v-for="(data, index) in skills" :key='index'>{{index}}. {{data.skill}}
+      </li> 
+    </ul>
+    <p v-if="skills.length >= 1"> You have more than one skill</p>
+    <p v-else>You have less than or equal to one skill</p>
+  </div>
+ </div> 
+</template>
+ 
+<script>
+export default {
+  name: 'practice',
+ data() {
+   return{
+     skills:[
+       {"skill" : "vue.js"},
+       {"skill" : "front end developer"},
+     ]
+   }
+ }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style  scoped>
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
